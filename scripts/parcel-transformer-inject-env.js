d986ff1b673f71a3abb294e452336186c5f001fb
David Orlea <david.orlea@gmail.com>
2026-04-26 13:25:55 +0200

    Add build version to meta data

---
+++ scripts/parcel-transformer-inject-env.js
@@ -0,0 +1,13 @@
+const { Transformer } = require('@parcel/plugin');
+
+module.exports = new Transformer({
+    async transform({ asset, options: { env } }) {
+        const code = await asset.getCode();
+        const result = code.replace(/{{{ ([^}]+) }}}/g, (match, envKey) => {
+            asset.invalidateOnEnvChange(envKey);
+            return env[envKey] === undefined ? match : env[envKey];
+        });
+        asset.setCode(result);
+        return [asset];
+    },
+});
